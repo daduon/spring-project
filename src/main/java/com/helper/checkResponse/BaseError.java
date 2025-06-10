192c60b065c62a3df78533b992cbb25059df1bb1
dounda <duonda1997@gmail.com>
2025-06-10 15:51:51 +0700

    Update: reponse data

--- src/main/java/com/helper/checkResponse/BaseError.java
+++ src/main/java/com/helper/checkResponse/BaseError.java
@@ -2,54 +2,14 @@ package com.helper.checkResponse;
 
 import java.time.LocalDateTime;
 
-import lombok.Builder;
+import lombok.*;
 
+@Data
 @Builder
-public record BaseError<T>(
-        Boolean status,
-        Integer code,
-        String message,
-        LocalDateTime timestamp,
-        T errors
-) {
-    
-    public static <T> BaseErrorBuilder<T> builder(){
-        return new BaseErrorBuilder<>();
-    }
-    public static class BaseErrorBuilder<T> {
-        private Boolean status;
-        private Integer code;
-        private String message;
-        private LocalDateTime timestamp;
-        private T errors;
-
-        public BaseErrorBuilder<T> status(Boolean status) {
-            this.status = status;
-            return this;
-        }
-
-        public BaseErrorBuilder<T> code(Integer code) {
-            this.code = code;
-            return this;
-        }
-
-        public BaseErrorBuilder<T> message(String message) {
-            this.message = message;
-            return this;
-        }
-
-        public BaseErrorBuilder<T> timestamp(LocalDateTime timestamp) {
-            this.timestamp = timestamp;
-            return this;
-        }
-
-        public BaseErrorBuilder<T> errors(T errors) {
-            this.errors = errors;
-            return this;
-        }
-
-        public BaseError<T> build() {
-            return new BaseError<>(status, code, message, timestamp, errors);
-        }
-    }
+public class BaseError<T>{
+    private Boolean status;
+    private Integer code;
+    private String message;
+    private LocalDateTime timestamp;
+    private T errors;
 }
