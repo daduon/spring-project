90160595ab935715934b8103882958808921a64a
dounda <duonda1997@gmail.com>
2025-06-13 08:18:31 +0700

    Test Merge

--- src/main/java/com/helper/HelperApplication.java
+++ src/main/java/com/helper/HelperApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelperApplication {
 
 	public static void main(String[] args) {
+		System.err.println("Starting Helper Application...");
 		SpringApplication.run(HelperApplication.class, args);
 	}
 
